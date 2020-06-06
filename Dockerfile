c9295372be3517847eb45db9279c2aee223c4ee0
Jyotin Lad <j.lad.uk@gmail.com>
2020-06-06 16:40:25 +0100

    Utlizing dotenv Package

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ COPY . /quote-notifier
 
 WORKDIR /quote-notifier
 
+RUN conda install -c conda-forge --file requirements.txt
+
 ENTRYPOINT ["python"]
 
 CMD ["quotenotifier.py", "-i", "1440"]
--- notifier.py
+++ notifier.py
@@ -1,27 +1,18 @@
-from os import path
-from json import load
+from dotenv import load_dotenv
+from os import getenv
 from smtplib import SMTP_SSL
 
 
 class Notifier:
 
     def __init__(self):
-        credentials = self._get_credentials()
-        user = credentials.get("user")
-        password = credentials.get("password")
+        load_dotenv()
+        user = getenv("GMAIL_USER")
+        password = getenv("GMAIL_PASSWORD")
 
         self.server = SMTP_SSL('smtp.gmail.com', 465)
         self.server.ehlo()
         self.server.login(user, password)
-
-    @staticmethod
-    def _get_credentials():
-        filename = "credentials.priv.json"
-        if not path.isfile(filename):
-            raise FileNotFoundError(filename)
-
-        with open(filename, "r") as fh:
-            return load(fh)
 
     def send(self, email, subject, body):
         message = f'Subject: {subject}\n\n{body}'
